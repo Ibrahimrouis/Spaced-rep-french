6ba6dd509999ce04018ef9d781720edf6348e05d
ibrahimrouis <ibrahim.rouis@yahoo.com>
2021-11-23 19:06:44 +0100

    adjusting

--- cronjob.py
+++ cronjob.py
@@ -6,6 +6,6 @@ from main import cronjob
 
 # Create an instance of scheduler and add function.
 scheduler = BlockingScheduler()
-scheduler.add_job(cronjob, "cron", hour=17,minute=13)
+scheduler.add_job(cronjob, "cron", hour=19,minute='10')
 
 scheduler.start()
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import smtplib
 from email.message import EmailMessage
 from datetime import date, timedelta
-from password import password
-
+from passwords import password
+import pickle
 
 def cronjob():
     """
@@ -10,26 +10,27 @@ def cronjob():
     The main cronjob to be run continuously.
     """
     print("Cron job is running")
-    logs = pickle.load(open('planning.pkl', 'rb'))
+    logs = pickle.load(open('./planning.pkl', 'rb'),encoding='latin1')
     today=date.today()
     result=''
     for key,value in logs.items():
         for k,v in logs[key].items():
             if v==today:
-                result=result+'\t'+key+'\t: '+k+'\n'
+                result=result+'\t'+str(key)+'\t: '+str(k)+'\n'
+    print(result)
     if result!='':
         msg = EmailMessage()
-        msg.set_content('Pages list for today, '+today+' : \n'+result)
+        msg.set_content('Pages list for today, '+str(today)+' : \n'+result)
         me = 'ibrahim.rouis@yahoo.com'
         you = 'ibrahim.rouis@yahoo.com'
         msg['Subject'] = f'Spaced Rep French'
         msg['From'] = me
         msg['To'] = you
-        login, password = 'ibrahim.rouis@yahoo.com',password
+        login, pswd = 'ibrahim.rouis@yahoo.com',password
 
         # Send the message via our own SMTP server.
         s = smtplib.SMTP_SSL(host='smtp.mail.yahoo.com')
         # s.set_debuglevel(1)
-        s.login(login, password)
+        s.login(login, pswd)
         s.sendmail(me, you, msg.as_string())
         s.quit()
