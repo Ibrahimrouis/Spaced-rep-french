bfc9f0153c5fa45dbe5fff93c2b75658942b5e4d
ibrahimrouis <ibrahim.rouis@yahoo.com>
2021-11-23 17:51:46 +0100

    adjusting cronjob file

--- cronjob.py
+++ cronjob.py
@@ -6,6 +6,6 @@ from main import cronjob
 
 # Create an instance of scheduler and add function.
 scheduler = BlockingScheduler()
-scheduler.add_job(cronjob, "cron", hour=17,minute=31)
+scheduler.add_job(cronjob, "cron", hour=16,minute=52)
 
 scheduler.start()
