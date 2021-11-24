a262bad562b89503b612d4ff08c1e1eb7db30628
ibrahimrouis <ibrahim.rouis@yahoo.com>
2021-11-24 11:00:59 +0100

    working with env

--- cronjob.py
+++ cronjob.py
@@ -6,6 +6,6 @@ from main import cronjob
 
 # Create an instance of scheduler and add function.
 scheduler = BlockingScheduler()
-scheduler.add_job(cronjob, "cron", second='*')
+scheduler.add_job(cronjob, "cron", hour=10,minute=5)
 
 scheduler.start()
